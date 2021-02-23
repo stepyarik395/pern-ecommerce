e604645b06e20b3065d3f4fb1216809453b5db63
Roman Yarovoy <roman@booma.co.il>
2021-02-23 11:11:27 +0200

    fix

--- server/models/models.js
+++ server/models/models.js
@@ -2,18 +2,18 @@ const sequelize = require('../db');
 const { DataTypes } = require('sequelize');
 
 const User = sequelize.define('user', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement },
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   email: { type: DataTypes.STRING, unique: true },
   password: { type: DataTypes.STRING },
   password: { type: DataTypes.STRING, defaultValue: 'USER' },
 });
 
 const Basket = sequelize.define('basket', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement },
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 });
 
 const BaketDevice = sequelize.define('basket_device', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement },
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 });
 
 const Device = sequelize.define('device', {
