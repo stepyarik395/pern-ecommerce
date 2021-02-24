8ce6c5e8116a9e035980b947c7e5c7058a85c50f
Roman Yarovoy <roman@booma.co.il>
2021-02-24 11:20:31 +0200

    try_send

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,7 +1,7 @@
 class DeviceController {
   async create(req, res) {}
   async getAll(req, res) {}
-  async getOne(req, res) {}
+  async get(req, res) {}
 }
 
 module.exports = new DeviceController();
